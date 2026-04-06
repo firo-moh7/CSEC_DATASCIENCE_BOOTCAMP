f14aeb3e30e8973d451bc9b4dbc12069fcb65f3b
firomsa-m <sabboonaaar11@gmail.com>
2026-04-06 13:57:26 +0300

    Implement basic arithmetic operations in calculator

---
+++ cli_calculator.py
@@ -0,0 +1,28 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    return x / y
+print("welcome to the simple calculator")
+choice = input()
+num1 = int(input())
+num2 = int(input())
+
+if choice == '1':
+    print("Result:", add(num1, num2))
+elif choice == '2':
+    print("Result:", subtract(num1, num2))
+elif choice == '3':
+    print("Result:", multiply(num1, num2))
+elif choice == '4':
+    print("Result:", divide(num1, num2))
+else:
+    print("Invalid choice!")
